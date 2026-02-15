81db7a129a25a9cb7d6d3e81471be4f66d46f0dc
Tansu Ozcelebi <tansu@kreamakina.com>
2026-02-15 12:01:01 +0300

    güncelleme

--- source/website/version.js
+++ source/website/version.js
@@ -1,7 +1,7 @@
 // Auto-generated version file - Do not edit manually
 export const KreaCAD_VERSION = {
-    version: '1.1.24',
-    build: 138,
-    timestamp: '2026-02-06T22:51:59.207Z',
-    fullVersion: 'v1.1.24.build.138'
+    version: '1.1.27',
+    build: 141,
+    timestamp: '2026-02-06T22:57:05.631Z',
+    fullVersion: 'v1.1.27.build.141'
 };
